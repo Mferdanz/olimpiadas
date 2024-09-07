6807379677615a8d19d4bbca708a4e7c49af523d
Mferdanz <mfdanzicourt@gmail.com>
2024-09-07 06:16:19 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,23 +1,22 @@
 let dados = [
+    
     {
         titulo : "Rayssa Leal",
         descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira uma nova geração de skatistas.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
-        tags:"skate fadinha brasileira nova geração"
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fadinha brasileira",
     },
     {
         titulo : "Rebeca Andrade",
         descricao : "A Rebeca Andrade é uma das maiores ginastas da história do Brasil.Sua determinação, talento e carisma a transformaram em um verdadeiro ícone do esporte nacional.",
-        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
-        tags:" ginasta historica determinação carisma icone ouro"
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta historica determinação carisma ouro",
     },
     {
         titulo : "Beatriz Souza",
         descricao : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de 2024. Sua técnica e força a levaram ao pódio, inspirando outros atletas e colocando o judô brasileiro em destaque.",
-        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
-        tags:" judô judo  2024 força pódio destaque"
-        
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo pódio destaque",
     },
+
 ];
-
-
